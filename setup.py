47d40881c38f12e4d8e77eee4e327cb3a44cd749
wyattis <wyattisrael@gmail.com>
2017-07-12 15:42:22 +0000

    Added setup.py for use in other projects

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='lastfm',
+      version='0.1',
+      description='Thin LastFM client',
+      author='Wyatt Israel',
+      author_email='wyatt.israel@gmail.com',
+      url='https://github.com/wyattis/python-lastfm',
+      py_modules=['lastfm']
+     )
