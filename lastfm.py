a49b6d2f4ef91de410671ba8b6ef2e3dae688cc5
wyattis <wyattisrael@gmail.com>
2017-07-12 02:45:47 +0000

    Added track facade, pagination and tests of all functionality. Track paging does not work due to searchTerms not showing up in search results.

--- lastfm.py
+++ lastfm.py
@@ -2,16 +2,23 @@ import requests as r
 
 base_url = "http://ws.audioscrobbler.com/2.0/"
 
+
 class RequestWrapper:
+    """
+    Used to wrap basic requests made by the requests lib that will add lastfm
+    api_keys and other required info.
+    """
     def __init__(self, api_key, api_secret):
         self.api_key = api_key
         self.api_secret = api_secret
     
     def get_raw(self, url):
+        """Just get response from the provided url. No edits to request are made"""
         response = r.get(url)
         return response.json()
     
     def get(self, method=None, params={}):
+        """Add the method, api_key and format to the request parameters"""
         params['method'] = method
         params['api_key'] = self.api_key
         params['format'] = 'json'
@@ -19,10 +26,12 @@ class RequestWrapper:
         return response.json()
 
 class Facade:
+    """Abstract Base Facade that is inherited from for all other Facades"""
     def __init__(self, wrapper):
         self.s = wrapper
 
 class ArtistFacade(Facade):
+    """Artist facade class to define methods that follow the API structure."""
     def getInfo(self, **kwargs):
         return self.s.get('artist.getinfo', kwargs)
     def getSimilar(self, **kwargs):
@@ -34,7 +43,19 @@ class ArtistFacade(Facade):
     def search(self, **kwargs):
         return self.s.get('artist.search', kwargs)
 
+class TrackFacade(Facade):
+    """Track facade class to define methods that follow the API structure."""
+    def getInfo(self, **kwargs):
+        return self.s.get('track.getInfo', kwargs)
+    def getSimilar(self, **kwargs):
+        return self.s.get('track.getSimilar', kwargs)
+    def getTopTags(self, **kwargs):
+        return self.s.get('track.getTopTags', kwargs)
+    def search(self, **kwargs):
+        return self.s.get('track.search', kwargs)
+
 class AlbumFacade(Facade):
+    """Album facade class to define methods that follow the API structure."""
     def getInfo(self, **kwargs):
         return self.s.get('album.getinfo', kwargs)
     def getTopTags(self, **kwargs):
@@ -42,8 +63,13 @@ class AlbumFacade(Facade):
     def search(self, **kwargs):
         return self.s.get('album.search', kwargs)
     
-class LastFM:
-    
+class LastFM():
+    """
+    Class that simulates the semantics of the lastfm API. Requires an API key
+    to be passed in or supplied as an env variable called LASTFM_API_KEY.
+    Making API requests then simply follows the semantics demonstrated in the
+    [LastFM](http://www.last.fm/api/) documentation.
+    """
     def __init__(self, api_key=None, api_secret=None):
         import os
         api_key = api_key if api_key else os.getenv('LASTFM_API_KEY')
@@ -51,9 +77,37 @@ class LastFM:
         self.wrapper = RequestWrapper(api_key, api_secret)
         self.album = AlbumFacade(self.wrapper)
         self.artist = ArtistFacade(self.wrapper)
+        self.track = TrackFacade(self.wrapper)
     
-    def next(self, response):
-        if 'next' in response:
-            return self.wrapper.get_raw(response['next'])
+    def next_page(self, response):
+        """Iterate through a response object with multiple pages."""
+        if 'results' in response:
+            r = response['results']
+            total_results = int(r['opensearch:totalResults'])
+            page =int(r['opensearch:Query']['startPage'])
+            start_index = int(r['opensearch:startIndex'])
+            limit = int(r['opensearch:itemsPerPage'])
+            search_terms = r['opensearch:Query']['searchTerms']
+            
+            # Check if we're going to exceed the number of results
+            if start_index + limit >= total_results:
+                return None
+            
+            if 'artistmatches' in r:
+                base = self.artist
+                query_type='artist'
+            elif 'albummatches' in r:
+                base = self.album
+                query_type='album'
+            elif 'trackmatches' in r:
+                base = self.track
+                query_type='track'
+                
+            p = {}
+            p[query_type] = search_terms
+            p['limit'] = limit
+            p['page'] = page + 1
+            return base.search(**p)
         else:
             return None
+            
--- test.py
+++ test.py
@@ -17,7 +17,8 @@ class AlbumTestCase(unittest.TestCase):
         tags = self.fm.album.getTopTags(mbid=self.album_mbid)
         self.assertEqual({}, tags)
     def test_search(self):
-        pass
+        res = self.fm.album.search(album=self.album_name)
+        self.assertTrue('results' in res)
 
 class ArtistTestCase(unittest.TestCase):
     def setUp(self):
@@ -39,6 +40,37 @@ class ArtistTestCase(unittest.TestCase):
     def test_search(self):
         res = self.fm.artist.search(artist=self.artist_name)
         self.assertTrue('results' in res)
+        
+class TrackTestCase(unittest.TestCase):
+    def setUp(self):
+        self.fm = LastFM()
+        self.artist_name = "Wintersleep"
+        self.artist_mbid = 'cda8e877-fe39-4939-8b09-045d68617367'
+        self.track_name = "Jaws of Life"
+        self.album_name = "Untitled"
+        self.track_mbid = 'e0b110b4-fef4-473a-9c42-207d3c92cae0'
+    def test_getInfo_by_mbid(self):
+        res = self.fm.track.getInfo(mbid=self.track_mbid)
+        self.assertEqual(self.track_mbid, res['track']['mbid'])
+    def test_getInfo_by_name(self):
+        res = self.fm.track.getInfo(artist=self.artist_name, track=self.track_name)
+        self.assertEqual(self.track_mbid, res['track']['mbid'])
+
+class PaginateTestCase(unittest.TestCase):
+    def setUp(self):
+        self.fm = LastFM()
+        self.query = 'winter'
+    def page(self, results):
+        new_results = self.fm.next_page(results)
+        self.assertTrue(results != None)
+        self.assertTrue(new_results != None)
+        self.assertTrue(results['results']['opensearch:startIndex'] != new_results['results']['opensearch:startIndex'])
+    def test_artist_page_results(self):
+        self.page(self.fm.artist.search(artist=self.query))
+    def test_album_page_results(self):
+        self.page(self.fm.album.search(album=self.query))
+    def test_track_page_results(self):
+        self.page(self.fm.track.search(track=self.query))
 
 if __name__ == '__main__':
     unittest.main()
